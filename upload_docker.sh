a78890e8cfac4f68e82498c3d8a984368ab01235
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-09-28 03:14:07 +0000

    Script to build, run and upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=timmystark/ml-microservice
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag timmystark/ml-microservice $dockerpath:latest
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
